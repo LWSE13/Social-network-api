cdc635a5aa81891cd5fb0cf725c76cd33e7cae66
LWSE13 <leewheeler888@gmail.com>
2024-05-27 20:42:32 +0100

    created and begun work on User model

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: 'Please enter a username!',
+            trim: true
+        },
+
+        email: {
+            type: String,
+            required: 'Please enter an email address!',
+            unique: true,
+            match: [/.+@.+\.(com|co\.uk)/, 'Please enter a valid e-mail address']
+        },
+
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+
+
+    }
+);
